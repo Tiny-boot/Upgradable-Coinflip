3c23ecf523aaa8f9ff202986a1547124c661c654
Tiny-boot <kathleenrrogan@gmail.com>
2025-02-01 22:03:52 +0100

    mod

--- src/Coinflip.sol
+++ src/Coinflip.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.28;
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import "./DauphineToken.sol"; // Import ERC20 token
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
@@ -14,36 +13,29 @@ error SeedTooShort();
 contract Coinflip is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     /// @custom:oz-upgrades-unsafe-allow constructor
     string public seed; //copied from the previous contract
-    DauphineToken public token;
 
     constructor() {
         _disableInitializers();
     }
 
-    function initialize(address initialOwner, address tokenAddress) public initializer {
+    function initialize(address initialOwner) public initializer {
         __Ownable_init(initialOwner);
         __UUPSUpgradeable_init();
         seed = "It is a good practice to rotate seeds often in gambling";
-        token = DauphineToken(tokenAddress);
-
     }
-
-    function RewardUser(address winner) internal {
-        token.mint(winner, 5 * 10 ** token.decimals());}
 
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
     /// @notice Checks user input against contract generated guesses
     /// @param Guesses is a fixed array of 10 elements which holds the user's guesses. The guesses are either 1 or 0 for heads or tails
     /// @return true if user correctly guesses each flip correctly or false otherwise
-    function UserInput(uint8[10] calldata Guesses,address player) external returns (bool) {
+    function UserInput(uint8[10] calldata Guesses) external view returns (bool) {
         uint8[10] memory flips = getFlips();
         for (uint256 i = 0; i < 10; i++) {
             if (Guesses[i] != flips[i]) {
                 return false;
             }
         }
-        RewardUser(player);
 
         return true;
     }
--- test/CoinflipUpgrade.t.sol
+++ test/CoinflipUpgrade.t.sol
@@ -5,27 +5,22 @@ import {Test, console2} from "forge-std/Test.sol";
 import {Coinflip} from "../src/Coinflip.sol";
 import {CoinflipV2} from "../src/CoinflipV2.sol";
 import {UUPSProxy} from "../src/Proxy.sol";
-import {DauphineToken} from "../src/DauphineToken.sol";
 
 contract CoinflipUpgradeTest is Test {
     Coinflip public game;
     CoinflipV2 public gameV2;
-    DauphineToken public token;
     UUPSProxy public proxy;
 
     Coinflip public wrappedV1;
     CoinflipV2 public wrappedV2;
 
     address owner = vm.addr(0x1);
-    address player = address(0x1234); // Dummy player address
 
     function setUp() public {
         // Set deployer to know address
 
         vm.startPrank(owner);
         // Initialize both versions of the contract
-        token = new DauphineToken(owner);
-        
         game = new Coinflip();
         gameV2 = new CoinflipV2();
 
@@ -35,20 +30,6 @@ contract CoinflipUpgradeTest is Test {
         // We need to cheat here a litte because the coin flip game is not deployed and the proxy
         // will not know how to access the game unless wrapped.
         wrappedV1 = Coinflip(address(proxy));
-        // Initialize the CoinflipV2 contract with the owner and token address
-        wrappedV1.upgradeToAndCall(
-            address(gameV2),
-            abi.encodeWithSignature("initialize(address,address)", owner, address(token))
-        );
-
-        // Wrap the proxy to interact with V2
-        wrappedV2 = CoinflipV2(address(proxy));
-
-        // Set CoinflipV2 as the minter in the DauphineToken contract
-        token.setMinter(address(wrappedV2));
-        console2.log("CoinflipV2 set as minter in DauphineToken");
-
-        vm.stopPrank();
     }
 
     /////////////////////////////////////////////////////
@@ -64,29 +45,18 @@ contract CoinflipUpgradeTest is Test {
     /////////////////////////////////////////////////////
 
     function test_V1Win() public {
-        vm.startPrank(player);
-
-        assertEq(wrappedV1.UserInput([1, 0, 0, 0, 1, 1, 1, 1, 0, 1], player), true);
-       
-        uint256 playerBalance = token.balanceOf(player);
-        assertEq(playerBalance, 5 * 10 ** token.decimals());
-
-        vm.stopPrank();
+        assertEq(wrappedV1.UserInput([1, 0, 0, 0, 1, 1, 1, 1, 0, 1]), true);
     }
 
     /////////////////////////////////////////////////////
     ////          Test seed rotation in V2           ////
     /////////////////////////////////////////////////////
     function test_Rotation() public {
-        vm.startPrank(owner);
-
         // Upgrade the proxy to V2 and wrap it
         wrappedV1.upgradeToAndCall(address(gameV2), "");
         wrappedV2 = CoinflipV2(address(proxy));
 
         wrappedV2.seedRotation("1234567890", 5);
         assertEq(wrappedV2.seed(), "6789012345");
-        vm.stopPrank();
-
     }
 }
